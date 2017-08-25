d735df28cdd2e2b76ae629e8276d2eef4b95e6eb
Doruk Ozturk <doruk.ozturk@kitware.com>
2017-08-25 13:55:54 -0400

    Add the information endpoint for inspecting a geotiff in girder

--- server/provider.py
+++ server/provider.py
@@ -10,20 +10,7 @@ from girder.utility.model_importer import ModelImporter
 class MapnikProvider(object):
     def __init__(self, layer, **kwargs):
         self.girder_file = kwargs['file']
-
-    def getAssetStorePath(self):
-
-        assetstore_model = ModelImporter.model('assetstore')
-        assetstore = assetstore_model.load(self.girder_file['assetstoreId'])
-        return assetstore['root']
-
-    def getLayerProjection(self):
-        file_path = os.path.join(self.getAssetStorePath(), self.girder_file['path'])
-        dataset = gdal.Open(file_path)
-        wkt = dataset.GetProjection()
-        proj = osr.SpatialReference()
-        proj.ImportFromWkt(wkt)
-        return proj.ExportToProj4()
+        self.info = kwargs['info']
 
     def addStyle(self, m, file):
         style = mapnik.Style()
@@ -32,22 +19,18 @@ class MapnikProvider(object):
         style.rules.append(rule)
         m.append_style('Raster Style', style)
         lyr = mapnik.Layer('GDAL Layer from TIFF file')
-        lyr.srs = self.getLayerProjection()
-        lyr.datasource = mapnik.Gdal(base=self.getAssetStorePath(),
-                                     file=file,
+        lyr.srs = self.info['srs']
+        lyr.datasource = mapnik.Gdal(base='',
+                                     file=self.info['path'],
                                      band=-1)
         lyr.styles.append('Raster Style')
         m.layers.append(lyr)
 
     def renderArea(self, width, height, srs, xmin, ymin, xmax, ymax, zoom):
         mapnik_map = mapnik.Map(width, height, srs)
-
         mapnik_map.zoom_to_box(mapnik.Box2d(xmin, ymin, xmax, ymax))
-
         img = mapnik.Image(width, height)
-
         self.addStyle(mapnik_map, self.girder_file['path'])
-
         mapnik.render(mapnik_map, img)
 
         return Image.frombytes('RGBA', (width, height), img.tostring())
--- server/rest.py
+++ server/rest.py
@@ -4,7 +4,11 @@ from girder.api import access
 from girder.api.describe import Description, autoDescribeRoute
 from girder.api.rest import Resource, setResponseHeader, rawResponse
 from girder.constants import AccessType
+from girder.utility.model_importer import ModelImporter
 
+
+import gdal
+import osr
 from TileStache import parseConfig, getTile
 from ModestMaps.Core import Coordinate
 
@@ -15,9 +19,11 @@ class kTile(Resource):
         self.resourceName = 'ktile'
 
         self.route('GET', (':id', ':z', ':x', ':y'), self.getTile)
+        self.route('GET', (':id', 'info'), self.getTiffInfo)
 
     def _getLayer(self, girder_file):
         layer_name = os.path.splitext(girder_file['name'])[0]
+        info = kTile._getInfo(girder_file)
         config_dict = {
             "cache":
             {
@@ -32,7 +38,8 @@ class kTile(Resource):
                     "provider":
                     {
                         "class": "girder.plugins.girder_ktile.provider:MapnikProvider",
-                        "kwargs": {"file": girder_file}
+                        "kwargs": {"file": girder_file,
+                                   "info": info}
                     },
                     "projection": "spherical mercator"
                 }
@@ -42,6 +49,31 @@ class kTile(Resource):
         layer = config.layers[layer_name]
 
         return layer
+
+    @staticmethod
+    def _getInfo(file):
+        info = {}
+        assetstore_model = ModelImporter.model('assetstore')
+        assetstore = assetstore_model.load(file['assetstoreId'])
+        path = os.path.join(assetstore['root'], file['path'])
+        dataset = gdal.Open(path)
+        geotransform = dataset.GetGeoTransform()
+        wkt = dataset.GetProjection()
+        proj = osr.SpatialReference()
+        proj.ImportFromWkt(wkt)
+        lrx = geotransform[0] + (dataset.RasterXSize * geotransform[1])
+        lry = geotransform[3] + (dataset.RasterYSize * geotransform[5])
+        info['path'] = path
+        info['driver'] = dataset.GetDriver().GetDescription()
+        info['pixel_size'] = geotransform[1], geotransform[5]
+        info['srs'] = proj.ExportToProj4()
+        info['size'] = dataset.RasterXSize, dataset.RasterYSize
+        info['bands'] = dataset.RasterCount
+        info['corners'] = {'ulx': geotransform[0],
+                           'uly': geotransform[3],
+                           'lrx': lrx,
+                           'lry': lry}
+        return info
 
 
     @access.public
@@ -60,3 +92,12 @@ class kTile(Resource):
         status_code, headers, tile = layer.getTileResponse(coordinates, 'png')
         setResponseHeader('Content-Type', headers.values()[0])
         return tile
+
+    @access.public
+    @autoDescribeRoute(
+        Description('Get information about a tiff file')
+        .modelParam('id', 'The ID of the file to be inspected.',
+                    model='file', level=AccessType.READ)
+    )
+    def getTiffInfo(self, file, params):
+        return kTile._getInfo(file)
