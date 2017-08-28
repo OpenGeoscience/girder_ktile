8a0750824bea408680474ec936c9d12f5f17faa2
Doruk Ozturk <doruk.ozturk@kitware.com>
2017-08-28 10:47:26 -0400

    Guard against not existing GDAL dependency

--- setup.py
+++ setup.py
@@ -3,16 +3,23 @@ from distutils.core import setup
 from pip.req import parse_requirements
 
 
+class GDALNotFoundException(Exception):
+    pass
+
+
 def get_gdal_version():
     cmd = subprocess.Popen(['gdal-config', '--version'], stdout=subprocess.PIPE)
     cmd_out, cmd_err = cmd.communicate()
+    if cmd.returncode != 0:
+        raise GDALNotFoundException('Could not find GDAL')
+
     return cmd_out
 
 install_reqs = parse_requirements('requirements.txt', session=False)
 reqs = [str(ir.req) for ir in install_reqs]
 
 gdal_version = get_gdal_version()
-reqs.append('GDAL=={}'.format(gdal_version))
+reqs.append('GDAL~={}'.format(gdal_version))
 
 setup(name='girder_ktile',
       version='0.0.1',
