2808b0990b67bc98a4a62884ebd3399cd3100570
Matthew Ma <matthew.ma@kitware.com>
2017-09-01 08:52:55 -0400

    Updates webpack helper to be work with latest girder;

--- webpack.helper.js
+++ webpack.helper.js
@@ -1,8 +1,7 @@
 module.exports = function (config) {
+    config.resolve = config.resolve || {};
+    config.resolve.alias = config.resolve.alias || {};
 	// Remove once geojs fix related issue
     config.resolve.alias['Hammer'] = 'hammerjs/hammer.js';
-    // Remove once jQuery query builder support webpack
-    config.resolve.alias['jQuery.extendext'] = 'jquery-extendext/jQuery.extendext.js';
-    config.resolve.alias['doT'] = 'dot/doT.js';
     return config;
 }
